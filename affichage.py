643a1e3c8c0f8769d10d45d5a4a303d510bfb115
lolodino77 <49190644+lolodino77@users.noreply.github.com>
2019-05-12 01:19:05 +0200

    Add files via upload

---
+++ affichage.py
@@ -0,0 +1,120 @@
+# -*- coding: utf-8 -*-
+#
+# Fichier : affichage.py
+# Date : février 2019 
+# Auteurs : Dang-Vu Laurent
+# 
+# Contient le squelette de la classe grille qui modélise une grille et le squelette de la classe Boule
+# qui modélise des cellules
+#
+#===========================================
+
+
+############# Quelques modules utiles #############
+
+from cellule import *
+import pygame
+from pygame.locals import *
+from random import randint
+
+############# Quelques fonctions utiles #############
+def separe(ligne):
+	ligne=ligne.strip("\n")
+	liste=ligne.split(",") # enlever un eventuel \n et/ou des espaces a la fin de la ligne
+	return(liste)
+
+def position_initiale():
+	listeBoules = []
+	for k in range(200):
+		boule_x = randint(20,400) 
+		boule_y = randint(20,650) 
+		nom = "X"+str(k)
+		couleur = "boule_jaune.png"
+		position = (boule_x,boule_y)
+		boule = Boule(nom,position,couleur)
+		listeBoules.append(boule)
+		# afficher_liste_boules(listeBoules)
+	return(listeBoules)
+	
+def position_initiale_fichier():
+	""" On récupère dans le fichier infos_boules.txt les positions initiales de chaque boules, et on crée toutes les boules sur ces positions """
+	# ...
+	f = open("infos_boules2.txt")
+	listeBoules = []
+	for ligne in f:
+		bouleListe = separe(ligne)
+		boule_x = int(bouleListe[1]) 
+		boule_y = int(bouleListe[2])
+		nom = bouleListe[0]
+		couleur = bouleListe[3]
+		position = (boule_x,boule_y)
+		boule = Boule(nom,position,couleur)
+		listeBoules.append(boule)
+	f.close()
+	return(listeBoules)		
+						
+
+
+############# Création du billard, affichage graphique #############
+
+# on initialise pygame
+pygame.init()
+
+# on crée une fenêtre que l'on stocke dans une variable nommée ecran. Les dimensions de la fenêtre sont celles stockées dans les variables LARGEUR et LONGUEUR (exprimées en pixels)
+ecran = pygame.display.set_mode((500,800))
+
+# on donne un joli titre à la fenêtre
+pygame.display.set_caption("Agregation de cellules")
+
+# on récupère le fond qui est l'image du billard avec ses bandes, que l'on met dans une variable nommée fond
+fond = pygame.image.load("billard.png")
+
+# création boule(s)
+position = (200,500)
+boule1 = Boule("X1",position,"boule_blanche.png")
+boule2 = Boule("X2",(332,600),"boule_jaune.png")
+
+listeBoules = position_initiale()
+
+# boucle de gestion des événements et d'affichage (dont on ne sort que si l'on ferme la fenêtre)
+onContinue = True
+horloge = pygame.time.Clock()
+while onContinue:
+
+	# Gestion des événements (on parcourt tous les événements, et si un évènement est de type QUIT, alors on ne continue plus)
+	# print("while")
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			onContinue = False
+	
+	# on colle le fond dans la fenêtre
+	ecran.blit(fond,(0,0)) # on colle le fond en position (0,0) qui sont les coordonnées du coin supérieur gauche de la fenêtre...
+		
+	#affichage des boules
+	boule1.affiche(ecran)
+	boule2.affiche(ecran)
+	
+	for boule in listeBoules:
+		boule.affiche(ecran)
+
+	# mise a jour de la grille, des positions des cellules et affichage graphique boule(s)
+	grille = Grille(listeBoules)
+	
+	# print("grille :\n",str(grille))
+	# print()
+	
+	# print(0)
+	# afficher_liste_boules(listeBoules)
+
+	# print(str(g))
+	# afficher_liste_boules(newPositions(listeBoules,grille))
+	listeBoules = nouvelles_positions(listeBoules,grille)
+	# listeBoules[2].distance(listeBoules[4])	
+	# print(1)
+	# afficher_liste_boules(listeBoules)
+
+	# on met à jour l'affichage
+	pygame.display.flip()
+
+	# on attend un petit peu, pour ne boucler que 25 fois maxi par seconde
+	# ...
---
+++ trouver_cases_adjacentes.py
@@ -0,0 +1,38 @@
+from pylab import *
+
+N_X = 4 #indice i ou j maximal
+
+T = array([[1,5,9,13],[2,6,10,14],[3,7,11,15],[4,8,12,16]])
+(i,j) = where(T==10)[0][0],where(T==10)[1][0]
+
+L_adjacents = []
+for k in range(1,17):
+    L_adjacents.append([])
+    i_c,j_c = where(T==k)[0][0]+1,where(T==k)[1][0]+1
+    L_adjacents[k-1].append((i_c,j_c))
+    for k1 in range(len(T)):#[0;3]
+        for k2 in range(len(T)):
+            i,j = k1+1,k2+1
+            ai = i_c-1
+            bi = i_c+1
+            aj = j_c-1
+            bj = j_c+1
+            if((ai<=i<=bi)and(aj<=j<=bj))and(0<i<=N_X)and(0<j<=N_X)and((i,j)!=(i_c,j_c)):
+                # print(k)
+                L_adjacents[k-1].append((i,j))
+
+L_adjacents_unD = []
+i = 0
+for elem in L_adjacents:
+    L_adjacents_unD.append([])
+    for couplet in elem:
+        ix = couplet[0]
+        iy = couplet[1]
+        k = ix + (iy-1)*N_X
+        L_adjacents_unD[i].append(k)
+    i += 1
+
+L_adjacents_unD = array(L_adjacents_unD)
+
+# pour chaque ligne, le premier element est celui dont on cherche les adjacents, et les autres sont les cases adjacentes a cet element
+print(L_adjacents_unD[3])#je veux les adjacents de k, il faut ecrire k-1 a cause du premier indice python a 0
